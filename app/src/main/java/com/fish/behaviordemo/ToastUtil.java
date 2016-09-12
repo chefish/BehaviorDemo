b08d9b91ebbd7c0b36669e440d0add09c2cc51cc
chefish <120065570@qq.com>
2016-09-12 19:41:29 +0800

    add  CollapsingActivity.java

---
+++ app/src/main/java/com/fish/behaviordemo/ToastUtil.java
@@ -0,0 +1,30 @@
+package com.fish.behaviordemo;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtil {
+
+	private static Toast mToast;
+
+	public static void showToast(Context ctx, String msg) {
+		if (ctx == null) {
+			return;
+		}
+
+		if (mToast == null) {
+			mToast = Toast.makeText(ctx.getApplicationContext(), msg, Toast.LENGTH_SHORT);
+		} else {
+			mToast.setText(msg);
+		}
+		mToast.show();
+	}
+
+	public static void releaseToast() {
+		if (mToast != null) {
+			mToast.cancel();
+			mToast = null;
+		}
+	}
+
+}
--- app/src/main/java/com/fish/behaviordemo/collapsing/CollapsingActivity.java
+++ app/src/main/java/com/fish/behaviordemo/collapsing/CollapsingActivity.java
@@ -2,6 +2,7 @@ package com.fish.behaviordemo.collapsing;
 
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,14 +14,19 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.fish.behaviordemo.R;
+import com.fish.behaviordemo.ToastUtil;
 
 
 public class CollapsingActivity extends AppCompatActivity {
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    COLLASP_MODE currentMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.collapsing_activity);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -43,13 +49,28 @@ public class CollapsingActivity extends AppCompatActivity {
             tv.setTextSize(40);
             ll.addView(tv);
         }
+        currentMode = COLLASP_MODE.SCROLL_DEFAULT;
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_collapsing, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        switch (currentMode) {
+            case SCROLL_DEFAULT:
+                menu.findItem(R.id.scroll).setChecked(true);
+                break;
+            case SCROLL_EXIT_COLLAPSE:
+                menu.findItem(R.id.scroll_exit).setChecked(true);
+                break;
+
+        }
         return true;
     }
 
@@ -61,10 +82,25 @@ public class CollapsingActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.scroll_exit) {
+            currentMode = COLLASP_MODE.SCROLL_EXIT_COLLAPSE;
+            AppBarLayout.LayoutParams params = (AppBarLayout.LayoutParams) collapsingToolbarLayout.getLayoutParams();
+            params.setScrollFlags(AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL | AppBarLayout.LayoutParams.SCROLL_FLAG_EXIT_UNTIL_COLLAPSED);
+            collapsingToolbarLayout.setLayoutParams(params);
+            ToastUtil.showToast(this, "scroll_exit_collapse");
+            return true;
+
+        } else if (id == R.id.scroll) {
+            currentMode = COLLASP_MODE.SCROLL_DEFAULT;
+            AppBarLayout.LayoutParams params = (AppBarLayout.LayoutParams) collapsingToolbarLayout.getLayoutParams();
+            params.setScrollFlags(AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL);
+            collapsingToolbarLayout.setLayoutParams(params);
+            ToastUtil.showToast(this, "scroll");
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    static enum COLLASP_MODE {SCROLL_DEFAULT, SCROLL_EXIT_COLLAPSE}
 }
