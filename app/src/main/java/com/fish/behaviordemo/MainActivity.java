de458ebb80d97bdf5dae1a4d449ca1a258c19b35
剑六 <xiaomin.yxm@alibaba-inc.com>
2017-09-29 17:21:48 +0800

    add some

--- app/src/main/java/com/fish/behaviordemo/MainActivity.java
+++ app/src/main/java/com/fish/behaviordemo/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
         appBar = (AppBarLayout) findViewById(R.id.barLayout);
 
+        //add some
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
