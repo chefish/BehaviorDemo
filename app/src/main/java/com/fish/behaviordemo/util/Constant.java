11d610465413cddf904d8cc1e2a72fc16cf771af
剑六 <xiaomin.yxm@alibaba-inc.com>
2017-09-29 17:20:08 +0800

    add in dd

--- app/src/main/java/com/fish/behaviordemo/util/Constant.java
+++ app/src/main/java/com/fish/behaviordemo/util/Constant.java
@@ -8,7 +8,7 @@ public class Constant {
             "Dianne Hackborn",
             "Chris Banes",
             "Saul Molinero"
-            aa
+            dd
     };
 
     public static String getName(int index) {
